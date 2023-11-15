cfbc0de178c93ea6a71f34a63fb411f377b71e06
Matthew Parker <matthewparker@hotmail.com.au>
2023-11-15 20:04:10 +1000

    initial commit

---
+++ src/Blazor.Metalama/LogAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Metalama.Framework.Aspects;
+using Metalama.Framework.Fabrics;
+
+namespace Blazor.Metalama;
+
+// Unusable until Metalama.Compiler properly supports Razor source generators, or using <UseRazorSourceGenerator>false</UseRazorSourceGenerator>.
+public class LogAttribute : OverrideMethodAspect
+{
+	public override dynamic? OverrideMethod()
+	{
+		Console.WriteLine("AnotherAttribute");
+		return meta.Proceed();
+	}
+}
